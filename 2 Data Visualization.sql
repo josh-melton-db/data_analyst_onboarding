51837f421b88c14517ff3ce3d85b261c659dde6f
josh-melton-db <josh-melton-db>
2023-11-29 13:29:33 +0000

    Added instructions for notebook dashboards

--- 2 Data Visualization.sql
+++ 2 Data Visualization.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 -- MAGIC %md
--- MAGIC Now that we've transformed our data into something interesting and saved it to a table, let's begin visualizing it! </br></br> Run the cell below and click the + button next to Table as shown below, and create a bar chart with the data  </br>
+-- MAGIC Now that we've transformed our data into something interesting and saved it to a table, let's begin visualizing it! </br></br> Run the cell below and click the + button next to Table as shown in the imagebelow, and create a bar chart visualization with the data. Make the X axis the factory id, the y axis the count, and group by defect. Click on the visualization's title to rename it to something more descriptive  </br>
 -- MAGIC
 -- MAGIC <img style="float: right" width="200" src="https://github.com/josh-melton-db/data_analyst_onboarding/blob/main/utils/images/create_visual.png?raw=true">
 
@@ -11,4 +11,21 @@ from josh_melton_onboarding.sensor_silver  -- <- change to the table created in 
 
 -- COMMAND ----------
 
+-- MAGIC %md
+-- MAGIC Click the down arrow next to the visualization's title and select "add to dashboard" and "add to new dashboard"
 
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC Click the + again and this time create a line chart visualization. Set the X axis to avg_heating_rate, y axis to sum of count, and group by defect. See if there are any spikes in defects for certain values of heating rate. </br></br>
+-- MAGIC Next let's add this to the dashboard you made before - click the down arrow next to the visualization's title, "add to dashboard", and select the dashboard you made in the last step
+
+-- COMMAND ----------
+
+select *
+from josh_melton_onboarding.sensor_bronze  -- <- change to the table created in the previous notebook
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC Try creating another visualization with the raw bronze data and adding that to the dashboard as well. Now you can share various insights into your data directly from Databricks! Click the "share" button in the top right of the dashboard to share with other groups or users. Click "schedule" to run the notebook and automatically update the dashboard
